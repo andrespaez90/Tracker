77e7525856bc8a76e70f7076147ef6fe13d0b777
Andres Paez <andres.paez@innso.co>
2015-08-29 12:46:09 -0500

    Initial Commit

---
+++ Tracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tracker
+//
+//  Created by INNOVATING SOFTWARE SAS on 29/08/15.
+//  Copyright (c) 2015 INNOVATING SOFTWARE SAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tracker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tracker
+//
+//  Created by INNOVATING SOFTWARE SAS on 29/08/15.
+//  Copyright (c) 2015 INNOVATING SOFTWARE SAS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
